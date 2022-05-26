eabfcfb654481318fbb2799011bb24a5a7151470
oburiatov <alexei.buriatov@gmail.com>
2022-05-26 07:58:57 +0300

    minor changes

--- lab1/backend/server/src/main.ts
+++ lab1/backend/server/src/main.ts
@@ -5,7 +5,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const fs = require('fs');
+  const keyFile  = fs.readFileSync(__dirname + '/../ssl/devopseek.key');
+  const certFile = fs.readFileSync(__dirname + '/../ssl/devopseek.crt');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    }
+  });
   app.enableCors({
     origin:'https://devopseek.me:30000',
     methods:['GET', 'POST'],
